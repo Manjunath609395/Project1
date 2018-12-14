4e61b043b6788943b8fa0da7766676236e28cf8d
Manjunath Avesani <manjunath.avesani@wkelms.com>
2018-12-14 17:13:39 +0530

    Updating Files in Story Branch

--- src/com/practice/github/HelloWorldSecond.java
+++ src/com/practice/github/HelloWorldSecond.java
@@ -4,7 +4,7 @@ public class HelloWorldSecond {
 
 	public static void main(String[] args) {
 		for(int i=0;i<10;i++) {
-			System.out.println("Im calling you out in a loop");
+			System.out.println("Im calling you out in a loop from child");
 		}
 
 	}
