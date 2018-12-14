2a297c8e5e96c85dd724139a80ee451c3c5cbfca
Manjunath Avesani <manjunath.avesani@wkelms.com>
2018-12-14 16:26:03 +0530

    Adding Files to Repo

---
+++ src/com/practice/github/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.practice.github;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Im calling you out");
+
+	}
+
+}
---
+++ src/com/practice/github/HelloWorldSecond.java
@@ -0,0 +1,12 @@
+package com.practice.github;
+
+public class HelloWorldSecond {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			System.out.println("Im calling you out in a loop");
+		}
+
+	}
+
+}
